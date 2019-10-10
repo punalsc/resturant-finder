64db65c99f0030bda43f4822b05fd9f477740d3e
Punal Chotrani <punalsc@gmail.com>
2019-10-10 21:47:15 +0100

    Testing user object

--- src/scripts/tests/functions.test.js
+++ src/scripts/tests/functions.test.js
@@ -2,9 +2,12 @@ import { functions } from '../functions';
 
 // Working with async data
 
-test('Users fetched should be John Davis', () => {
+test('Users fetched should be John Davis and his items', () => {
 	return functions.fetchFunc().then(data => {
 		expect(data[0].name).toBe('John Davis');
 		expect(data[0].wont_eat[0]).toEqual('Fish');
+		expect(data[0].drinks).toEqual(
+			expect.arrayContaining(['Cider', 'Rum', 'Soft drinks'])
+		);
 	});
 });
