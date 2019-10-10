efe0011a90b5f0c4fb33c4f7a80c02bc1077d92b
Punal Chotrani <punalsc@gmail.com>
2019-10-10 22:06:58 +0100

    Refactored test as async await

--- src/scripts/tests/functions.test.js
+++ src/scripts/tests/functions.test.js
@@ -1,13 +1,11 @@
 import { functions } from '../functions';
 
 // Working with async data
-
-test('Users fetched should be John Davis and his items', () => {
-	return functions.fetchFunc().then(data => {
-		expect(data[0].name).toBe('John Davis');
-		expect(data[0].wont_eat[0]).toEqual('Fish');
-		expect(data[0].drinks).toEqual(
-			expect.arrayContaining(['Cider', 'Rum', 'Soft drinks'])
-		);
-	});
+test('Users fetched should be John Davis and his items', async () => {
+	const data = await functions.fetchFunc();
+	expect(data[0].name).toBe('John Davis');
+	expect(data[0].wont_eat[0]).toEqual('Fish');
+	expect(data[0].drinks).toEqual(
+		expect.arrayContaining(['Cider', 'Rum', 'Soft drinks'])
+	);
 });
